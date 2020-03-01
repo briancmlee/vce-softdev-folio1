cc726276b6fe9059961e14cffac1c0cc1aba8379
Brian Lee <briancmlee23@gmail.com>
2020-03-01 13:41:40 +1100

    Updates

---
+++ precache-manifest.da6a61a8c4beaefba32808f405937266.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8399407fde02f327e55ee23a6370a2",
+    "url": "/vce-softdev-folio1/index.html"
+  },
+  {
+    "revision": "4403513e42e85236c1dd",
+    "url": "/vce-softdev-folio1/static/css/main.e2431350.chunk.css"
+  },
+  {
+    "revision": "0add03aa912a254487a3",
+    "url": "/vce-softdev-folio1/static/js/2.023c121e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/vce-softdev-folio1/static/js/2.023c121e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4403513e42e85236c1dd",
+    "url": "/vce-softdev-folio1/static/js/main.99d7d94e.chunk.js"
+  },
+  {
+    "revision": "2b866795f533a962ddc9",
+    "url": "/vce-softdev-folio1/static/js/runtime-main.9439a8f0.js"
+  },
+  {
+    "revision": "a8e5a00f6862d8108876a8afbf0091ab",
+    "url": "/vce-softdev-folio1/static/media/apple.a8e5a00f.png"
+  },
+  {
+    "revision": "19a0e8bc5fe88b45f7b4691df5a45c45",
+    "url": "/vce-softdev-folio1/static/media/car.19a0e8bc.png"
+  },
+  {
+    "revision": "505f94e4a27f1a8aa03102cdf05906bb",
+    "url": "/vce-softdev-folio1/static/media/egg.505f94e4.png"
+  }
+]);
